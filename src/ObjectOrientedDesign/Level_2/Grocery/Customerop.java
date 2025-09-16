08d06414ea38be83dca95372eaa0280b2edceb41
kuldeepchahar1 <kuldeepchahar161@gmail.com>
2025-09-16 12:20:57 +0530

    Object oriented design practice Questions

---
+++ src/ObjectOrientedDesign/Level_2/Grocery/Customerop.java
@@ -0,0 +1,19 @@
+package ObjectOrientedDesign.Level_2.Grocery;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customerop {
+    private String name;
+    private List<Product> products = new ArrayList<>();
+
+    public Customerop(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+    public List<Product> getProducts() { return products; }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+}
